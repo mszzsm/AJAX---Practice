df94a1ddbdfb0ec5b76bd91bf6173eafc4acc09f
Michał Sztefanica <mikeshtefanitsa@gmail.com>
2019-02-13 23:02:15 +0100

    second commit

--- ajax.js
+++ ajax.js
@@ -1,15 +1,19 @@
 window.onload = function() {
     document.querySelector('#shop_ip').onclick = function (){
-        ajaxGet('ip.php');
+        
+        ajaxGet('ip.php', function(data){
+            document.querySelector('#myip').innerHTML = data;
+        });
     }
 }
 
-function ajaxGet(url){
+function ajaxGet(url, callback){
+    var f = callback || function (data) {}
     var request = new XMLHttpRequest();
 
     request.onreadystatechange = function() {
         if(request.readyState == 4 && request.status == 200) {
-            document.querySelector ('#myip').innerHTML = request.responseText;
+           f  (request.responseText);
         };
     }
 
