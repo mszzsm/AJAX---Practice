8f34e7d3adfe210286ae27cb1d3cae7f4063cf25
Michał Sztefanica <mikeshtefanitsa@gmail.com>
2019-02-13 22:49:16 +0100

    first ajax responce

--- ajax.js
+++ ajax.js
@@ -1,1 +1,18 @@
-ajax.js
+window.onload = function() {
+    document.querySelector('#shop_ip').onclick = function (){
+        ajaxGet('ip.php');
+    }
+}
+
+function ajaxGet(url){
+    var request = new XMLHttpRequest();
+
+    request.onreadystatechange = function() {
+        if(request.readyState == 4 && request.status == 200) {
+            document.querySelector ('#myip').innerHTML = request.responseText;
+        };
+    }
+
+    request.open('GET', url);
+    request.send();
+}
---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <script src="ajax.js"></script>
+</head>
+<body>
+   <input id="shop_ip" type="button" value="Show my IP"> </button>
+   <div id="myip"> </div>
+   <script src="script.js"> </script>
+    
+</body>
+</html>
